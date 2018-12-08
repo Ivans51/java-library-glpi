bf9031de94f991c0f477719c67d967e7b19bf068
Flyve MDM <ml-flyvemdm@teclib.com>
2018-12-08 00:32:38 +0000

    ci(build): test
    Signed-off-by: Ivan Del Pino <idelpino@teclib.com>

---
+++ ci/scripts/ci_deploy_production.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# ---------------------------------------------------------------------
+#
+#  LICENSE
+#
+#  This file is part of the GLPI API Client Library for Java,
+#  a subproject of GLPI. GLPI is a free IT Asset Management.
+#
+#  GLPI is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  GLPI is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  --------------------------------------------------------------------
+#  @author    Ivan Del Pino - <idelpino@teclib.com>
+#  @copyright (C) 2017 Teclib' and contributors.
+#  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+#  @link      https://github.com/glpi-project/java-library-glpi
+#  @link      http://www.glpi-project.org/
+#  --------------------------------------------------------------------
+#
+# send to google play
+
+GH_COMMIT_MESSAGE=$(git log --pretty=oneline -n 1 $CIRCLE_SHA1)
+
+if [[ $GH_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version"* && $GH_COMMIT_MESSAGE != *"ci(build): release version"* ]]; then
+
+# run update version script
+ci/scripts/ci_updateversion.sh
+
+# run push changes script
+ci/scripts/ci_push_changes.sh
+
+# run github release script
+# ci/scripts/ci_github_release.sh
+
+# run bintray script
+ci/scripts/ci_bintray.sh
+
+fi
